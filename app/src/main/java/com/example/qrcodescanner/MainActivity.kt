bb69bf1a06cb6aacfde77390740d6b5c930b2d2e
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-11-11 13:29:41 -0300

    Verify whether QRCode module is available, install it when necessary

--- app/src/main/java/com/example/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.qrcodescanner
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,23 +26,19 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.qrcodescanner.ui.theme.QRCodeScannerTheme
+import com.google.android.gms.common.moduleinstall.ModuleInstall
+import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val options = GmsBarcodeScannerOptions.Builder()
-            .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE)
-//            .enableAutoZoom()
-            .build()
+        val context = this.applicationContext
 
-        val scanner = GmsBarcodeScanning.getClient(this, options)
         setContent {
             QRCodeScannerTheme {
 
@@ -53,25 +52,69 @@ class MainActivity : ComponentActivity() {
 
                         scannedValue = scannedValue,
                         startScan = {
-                            scanner.startScan()
-                                .addOnSuccessListener { barcode ->
+                            if (isModuleAvailable(context)) {
+                                scanQRCode(context, scannedValue)
+                            } else {
+                                val moduleInstall = ModuleInstall.getClient(context)
+                                val moduleInstallRequest = ModuleInstallRequest.newBuilder()
+                                    .addApi(GmsBarcodeScanning.getClient(context))
+                                    .build()
 
-                                    scannedValue.value = barcode.rawValue
-                                }
-                                .addOnCanceledListener {
-
-                                }
-                                .addOnFailureListener { e ->
-                                    e.printStackTrace()
-                                }
+                                moduleInstall.installModules(moduleInstallRequest)
+                                    .addOnSuccessListener { response ->
+                                        if (response.areModulesAlreadyInstalled()) {
+                                            // Module already installed, proceed with scanning
+                                            scanQRCode(context, scannedValue)
+                                        } else {
+                                            // Module was just installed, wait briefly then scan
+                                            Handler(Looper.getMainLooper()).postDelayed({
+                                                scanQRCode(context, scannedValue)
+                                            }, 1000)
+                                        }
+                                    }
+                                    .addOnFailureListener { e ->
+                                        // Handle installation failure
+                                    }
+                            }
                         },
                     )
                 }
             }
         }
     }
+}
 
+fun isModuleAvailable(context: Context): Boolean {
+    val gmsBarcodeScanner = GmsBarcodeScanning.getClient(context)
+    val moduleInstall = ModuleInstall.getClient(context)
+    var isAvailable = false
 
+    moduleInstall.areModulesAvailable(gmsBarcodeScanner).addOnSuccessListener{
+        isAvailable = it.areModulesAvailable()
+    }
+
+    return isAvailable
+}
+
+fun scanQRCode(context: Context, scannedValue: MutableState<String?> ) {
+    val options = GmsBarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE)
+        .enableAutoZoom()
+        .build()
+
+    val scanner = GmsBarcodeScanning.getClient(context, options)
+    scanner.startScan()
+        .addOnSuccessListener { barcode ->
+            scannedValue.value = barcode.rawValue.toString()
+        }
+        .addOnCanceledListener {
+            scannedValue.value = "Scan Cancelled"
+        }
+        .addOnFailureListener { e ->
+            e.printStackTrace()
+            scannedValue.value = "Scan Failed\n Error: ${e.message}"
+        }
 }
 
 @Composable
@@ -105,6 +148,4 @@ fun MainScreen(
         )
     }
 }
-
-
 
